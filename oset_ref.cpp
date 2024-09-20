8153165058e41cc12b12f39d41bc7cf7522f9eec
Alex Zhang <122637724+alexyzha@users.noreply.github.com>
2024-09-20 16:29:26 -0700

    added oset reference for later

---
+++ oset_ref.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+using oset = tree<A,null_type,less<A>,rb_tree_tag,tree_order_statistics_node_update>;
+
+// order_of_key
+// find_by_order
